02d7af019a7d1c4b0bee0b843a69e5bbf7b03339
Hammad Nisar <95129765+hammadimalik3@users.noreply.github.com>
2025-06-23 19:55:16 +0000

    variables done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountID = 1234555
+var accountEmail = "hammad@gmail.com"
+let accountPassword = "hamm13213"
+accountCity = "khasab"
+let accountState;
+
+//accountID = 2
+
+accountEmail = "hadi@gmail.com"
+accountPassword = "23456"
+accountCity = "rawalpindi"
+
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState])
