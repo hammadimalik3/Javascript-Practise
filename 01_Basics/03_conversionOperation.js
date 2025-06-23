a2ba685e2fbbb1061d3683aa1495b4c3d7b6fbec
Hammad Nisar <95129765+hammadimalik3@users.noreply.github.com>
2025-06-23 21:02:49 +0000

    conversion operation done

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,43 @@
+let score = "Hammad"
+
+//console.log(typeof score)
+//console.log(typeof (score))
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+//let isLoggedIn = ""
+
+//let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn)
+
+//let someNumber = 32
+
+//let stringNumber = String(someNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+//***************************** Opartaions *******************
+
+// let value = 3
+// let negValue = -value
+// //console.log(negValue)
+
+// let str1 = "Rohan"
+// let str2 = " Hammad"
+
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log(1 + "2")
+// console.log("1" + 2)
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log(+true)
+// console.log(+"")
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
