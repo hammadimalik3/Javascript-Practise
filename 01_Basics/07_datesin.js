9f5d07e5672ebb770e053f52de34e12c9ae99f33
Hammad Nisar <95129765+hammadimalik3@users.noreply.github.com>
2025-06-25 10:10:54 +0000

    dates in js done

---
+++ 01_Basics/07_datesin.js
@@ -0,0 +1,40 @@
+// Dates
+// let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreateDate = new Date(2025,5,25);
+// let myCreateDate = new Date(2025,5,25,1,55);
+// let myCreateDate = new Date("2025-06-25");
+// let myCreateDate = new Date("06-25-2025")
+// console.log(myCreateDate.toLocaleString());
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth());
+// console.log(newDate.getDay());
+
+
+// console.log(`${newDate.getDay()} and the time ${newDate.getTime()}`);
+
+
+newDate.toLocaleString('default', {
+   weekday: "short",
+    
+ })
+
+console.log(newDate);
+
+
+
+
+
