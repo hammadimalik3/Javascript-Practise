077e4abdd53d4f66dd5c0483bbccb0a14f441729
Hammad Nisar <95129765+hammadimalik3@users.noreply.github.com>
2025-06-25 19:39:53 +0000

    arrays pt1 done

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,38 @@
+ const myarr = [1,2,3,4,5,6]
+// console.log(myarr);
+
+// const myarr2 = new Array (1,2,3,4,5)
+// console.log(myarr2[1]);
+
+// myarr.push(7);
+// myarr.pop();
+// console.log(myarr);
+
+// myarr.unshift(7);
+// myarr.shift();
+// console.log(myarr);
+
+// console.log(myarr.includes(5));
+// console.log(myarr.indexOf(4));
+
+// const newarr = myarr.join() // convert array elements to string
+// console.log(myarr);
+// console.log(newarr);
+
+// slice , splice
+
+console.log("A", myarr);
+const myn1 = myarr.slice(1,3)
+console.log(myn1);
+
+console.log("B", myarr);
+const myn2 = myarr.splice(1,3)
+console.log(myn2);
+
+console.log("C", myarr);
+
+
+
+
+
+
