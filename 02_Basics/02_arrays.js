66af070655581365d74e7c642cd2475e29d8115a
Hammad Nisar <95129765+hammadimalik3@users.noreply.github.com>
2025-06-25 19:59:00 +0000

    arrays pt2 done

---
+++ 02_Basics/02_arrays.js
@@ -0,0 +1,33 @@
+const marvel_heros = ["thor", "ironman", "spiderman"]
+const dc_heros = ["superman", "captain", "superman"]
+const war_heros = ["iran", "pakistan"]
+
+// marvel_heros.push(dc_heros);
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const heros = marvel_heros.concat(dc_heros)
+// console.log(heros);
+
+// const all_heros = [...marvel_heros, ...dc_heros, ...war_heros]
+// console.log(all_heros); // concate the all arrays in one new array
+
+// const another_array = [1,2,3,[4,5,6],7,[4,5,[5,6]]]
+// const real_array = another_array.flat(Infinity)
+// console.log(real_array); // drop all arrays elements in one new array
+
+
+// console.log(Array.isArray("rohan")); // check true or false
+// console.log(Array.from("rohan")); // convert it to array
+// console.log(Array.from({name:'rohan'})); //will give empty bec it will only take key or value for object
+
+let score1 = 100
+let score2 = 300
+let score3 = 500
+
+console.log(Array.of(score1,score2,score3)); // combine all arrays elements in one array
+
+
+
+
